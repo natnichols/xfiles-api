08c07254d9ccca86c2973f4aea31f853cf912927
NatNichols <natalie.e.nichols@gmail.com>
2024-02-05 15:04:28 -0500

    import and mount xfilesRouter

---
+++ controllers/xfiles.js
@@ -0,0 +1,5 @@
+import { Xfile } from '../models/xfile.js'
+
+export {
+  
+}
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import './config/database.js'
 
 // import routers
 import { router as indexRouter } from './routes/index.js'
+import { router as xfilesRouter } from './routes/xfiles.js'
 
 // create the express app
 const app = express()
@@ -18,6 +19,7 @@ app.use(express.json())
 
 // mount imported routes
 app.use('/', indexRouter)
+app.use('/api/xfiles', xfilesRouter)
 
 // handle 404 errors
 app.use(function (req, res, next) {
