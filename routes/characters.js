b736bc3e251f68092cba5d41600b769710640deb
NatNichols <natalie.e.nichols@gmail.com>
2024-02-06 22:29:36 -0500

    stub up and mount characters router, create controllers/characters.js

---
+++ routes/characters.js
@@ -0,0 +1,16 @@
+import { Router } from 'express'
+import * as charactersCtrl from '../controllers/characters.js'
+
+const router = Router()
+
+// ========== Public Routes ===========
+// GET to localhost:3001/api/characters
+
+// POST to localhost:3001/api/characters
+
+// DELETE to localhost:3001/api/characters/:characterId
+
+
+export {
+  router
+}
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import './config/database.js'
 // import routers
 import { router as indexRouter } from './routes/index.js'
 import { router as xfilesRouter } from './routes/xfiles.js'
+import { router as charactersRouter } from './routes/characters.js'
 
 // create the express app
 const app = express()
@@ -20,6 +21,7 @@ app.use(express.json())
 // mount imported routes
 app.use('/', indexRouter)
 app.use('/api/xfiles', xfilesRouter)
+app.use('/api/characters', charactersRouter)
 
 // handle 404 errors
 app.use(function (req, res, next) {
