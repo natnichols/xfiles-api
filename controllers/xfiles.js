977a29870c8d55b76f17577513975dd99ec9082a
NatNichols <natalie.e.nichols@gmail.com>
2024-02-06 23:15:42 -0500

    write createQuote route and controller

--- controllers/xfiles.js
+++ controllers/xfiles.js
@@ -1,3 +1,4 @@
+import { Character } from '../models/character.js'
 import { Xfile } from '../models/xfile.js'
 
 export async function create(req, res) {
@@ -24,6 +25,28 @@ async function deleteXfile(req, res) {
   try {
     const xfile = await Xfile.findByIdAndDelete(req.params.xfileId)
     res.json(xfile)
+  } catch (err) {
+    console.log(`🚨`, err)
+    res.status(500).json(`🚨`, err)
+  }
+}
+
+export async function createQuote(req, res) {
+  try {
+    const xfile = await Xfile.findById(req.params.xfileId)
+    xfile.quotes.push(req.body)
+    await xfile.save()
+
+    // find newly created quote
+    const newQuote = xfile.quotes[xfile.quotes.length - 1]
+    // temporarily append character object to newQuote.character
+    const character = await Character.findById(req.character.name)
+    newQuote.character = character
+    // temporarily append xfile object to newQuote.episode
+    const episode = await Xfile.findById(req.episode.name)
+    newQuote.episode = episode
+    // respond with new quote
+    res.json(newQuote)
   } catch (err) {
     console.log(`🚨`, err)
     res.status(500).json(`🚨`, err)
--- routes/xfiles.js
+++ routes/xfiles.js
@@ -8,6 +8,8 @@ const router = Router()
 router.get('/', xfilesCtrl.index)
 // POST to localhost:3001/api/xfiles
 router.post('/', xfilesCtrl.create)
+// POST to localhost:3001/api/xfiles/:xfileId/quotes
+router.post('/:xfileId/quotes', xfilesCtrl.createQuote)
 // DELETE to localhost:3001/api/xfiles/:xfileId
 router.delete('/:xfileId', xfilesCtrl.delete)
 
